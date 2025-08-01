1d204dda969e182f64b1ffc68d440571e65f3596
Aldi Pranata <aldyjrz@gmail.com>
2025-08-01 14:45:47 +0700

    Update hehe.lua

--- hehe.lua
+++ hehe.lua
@@ -15,12 +15,12 @@ local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/Siri
 -- Window
 local Window = Rayfield:CreateWindow({
     Name = "Fish It Script",
-    LoadingTitle = "Fish It Script",
-    LoadingSubtitle = "by Prince",
-    Theme = "Amethyst",
+    LoadingTitle = "Fish It ",
+    LoadingSubtitle = "by AldyToi",
+    Theme = "Default",
     ConfigurationSaving = {
         Enabled = true,
-        FolderName = "QuietXHub",
+        FolderName = "AldyToi",
         FileName = "FishIt"
     },
     KeySystem = false
